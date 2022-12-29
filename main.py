c2ee85fb13f1bdbe38f1724291c700cdc514125a
riem659 <121521891+riem659@users.noreply.github.com>
2022-12-29 16:25:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
-from telethon import TelegramClient, sync
+from telethon import TelegramClient
 from telethon.tl.functions.account import UpdateUsernameRequest
 import threading, requests, time, random
 
 # you should use your own api_hash and api_id
-api_hash = 'api_hash' 
+api_hash = 'your_api_hash' 
 api_id = 123
-
 
 state = False
 time1 = time.time()
 new_username = ''
+result = None
 
 def check_username():
-    global new_username, state, time1
+    global new_username, state, time1, result
 
     while True:
         r = requests.get('https://t.me/' + new_username)
 
-        if 'tgme_action_button_new shine' in r.text:
+        if 'shine' in r.text:
+            result = 0
+
+        if result != 0:
             time1 = time.time()
             state = True
             break
-        elif r.status_code == 200 and 'twitter:image' in r.text:
+        elif r.status_code == 200 and result == 0:
             print("checking username {0}".format('.'*random.randrange(1, 5)+' '*5), end="\r")
         else:
             print('no response from telegram')
@@ -38,7 +41,7 @@ t.start()
 while True:
     if state == True:
         client(UpdateUsernameRequest(new_username))
-        print('username chenged in {0:.2f} seconds after last check.'.format(time.time() - time1))
+        print('username changed in {0:.2f} seconds after last check.'.format(time.time() - time1))
         break
 
 input('press enter to exit')
